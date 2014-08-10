235c226fa4f9a3a3b0c3ca373d64b103f3318f93
Evan Krambuhl <thekrambuhl@gmail.com>
2014-08-10 02:55:46 -0700

    define heroku port

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 
 var server = express();
 server.use(express.static('./dist'));
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
 
-console.log('Server started: http://localhost:8080');
+console.log('Server started: http://localhost:' + (process.env.PORT || 8080));
