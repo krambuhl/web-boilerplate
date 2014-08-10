9c9906fbbc5f10bd1f95ac260521728fe961c0a3
Evan Krambuhl <thekrambuhl@gmail.com>
2014-08-10 15:55:48 -0700

    document tasks + minor version bump

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,12 @@ var path  = require('path');
 var sequence = require('run-sequence');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
-var clean = require('gulp-clean');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
+
+// build / dist
+var clean = require('gulp-clean');
 var fileinclude = require('gulp-file-include');
-var imagemin = require('gulp-imagemin');
-var pngcrush = require('imagemin-pngcrush');
 
 // js tasks
 var bowerFiles  = require('bower-files')();
@@ -22,6 +22,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
 // image/svg tasks
+var imagemin = require('gulp-imagemin');
 
 // css tasks
 var sass = require('gulp-sass');
@@ -40,16 +41,26 @@ var dir = {
 };
 
 
+// Task `empty`
+// empties the dist folder before each startup
+// > input `dist` folder
+//  - clean
 gulp.task('empty', function () {
   return gulp.src(dir.dist, {read: false})
     .pipe(clean());
 });
 
-// __styles__ task:
-// - sass
-// - autoprefixer
-// - media query combiner
-// - css minifier
+
+// Task `styles`
+// compiles stylesheet and optimises file
+// > input `sass/style.scss`
+//  - sass
+//  - autoprefixer
+//  - media query combiner
+//  ==> output `style.css`
+//  ==> output `style.css.map`
+//  - css minifier
+//  ==> output `style.min.css
 gulp.task('styles', function() {
   return gulp.src(path.join(dir.src, 'sass/style.scss'))
     .pipe(sourcemaps.init())
@@ -64,6 +75,13 @@ gulp.task('styles', function() {
 });
 
 
+// Task `app`
+// compiles app script and optimises file
+// > input `app/app.js`
+//  - fileinclude
+//  ==> output `app.js` w/ sourcemaps
+//  - uglify
+//  ==> output `app.min.js
 gulp.task('app', function() {
   return gulp.src(path.join(dir.src, 'app/app.js'))
     .pipe(sourcemaps.init())
@@ -75,6 +93,14 @@ gulp.task('app', function() {
     .pipe(gulp.dest(dir.scripts));
 });
 
+
+// Task `lib`
+// combines bower scripts and optimises file
+// > input bowerFiles.js
+//  - concat
+//  ==> output `lib.js`
+//  - uglify
+//  ==> output `lib.min.js
 gulp.task('lib', function() {
   return gulp.src(bowerFiles.js)
     .pipe(concat('lib.js'))
@@ -85,32 +111,20 @@ gulp.task('lib', function() {
 });
 
 
+// Task `pull-data`
+// pull any external data needed before
+// assembling html pages
 gulp.task('pull-data', function() {
   return;
 });
 
 
-// __build__ task:
-// compiles static templates with enviornment data
-//
-// - static handlebars
-//   + output: various files to './dist/'
-gulp.task('build-pages', function() {
-  return;
-});
-
-
-gulp.task('copy-images', function() {
-  return gulp.src(path.join(dir.src, 'images/**/*.{jpg,jpeg,png,gif}'))
-    .pipe(imagemin({ use: [pngcrush()] }))
-    .pipe(gulp.dest(path.join(dir.assets, 'images')));
-});
-
-gulp.task('copy-fonts', function() {
-  return gulp.src(path.join(dir.src, 'fonts/**/*'))
-    .pipe(gulp.dest(path.join(dir.assets, 'fonts')));
-});
-
+// Task `post-iconizr`
+// creates a includable partial from the
+// iconizr fragment before assembling html pages
+// > input `sprite-loader-fragment.html`
+//  - replace (fix loader pathing)
+//  ==> output `partials/iconizr-fragment.hbs`
 gulp.task('post-iconizr', function() {
  return gulp.src(path.join(dir.assets, 'svg/sprite-loader-fragment.html'))
   .pipe(replace(/\/dist/ig, ''))
@@ -119,10 +133,37 @@ gulp.task('post-iconizr', function() {
 });
 
 
-// __watch__ task:
+// Task `copy-images`
+// minimizes and copies images
+// > input `images` folder (jpg/png/gif)
+//  - imagemin
+//  - pngcrush
+//  ==> output to `dist/assets`
+gulp.task('copy-images', function() {
+  return gulp.src(path.join(dir.src, 'images/**/*.{jpg,jpeg,png,gif}'))
+    .pipe(imagemin({ 
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest(path.join(dir.assets, 'images')));
+});
+
+
+// Task `copy-fonts`
+// copies fonts folder to dist
+// > input `fonts` folder
+//  ==> output to `dist/assets`
+gulp.task('copy-fonts', function() {
+  return gulp.src(path.join(dir.src, 'fonts/**/*'))
+    .pipe(gulp.dest(path.join(dir.assets, 'fonts')));
+});
+
+
+// Task `watch`
+// run various tasks on file changes
 gulp.task('watch', function () {
-  // run `styles` task on css file changes
-  gulp.watch(path.join(dir.src, 'css/**/*.{css,sass,scss}'), ['styles']);
+  // run `styles` task on css file changes in `./
+  gulp.watch(path.join(dir.src, 'sass/**/*.{css,sass,scss}'), ['styles']);
 
   // run `app` task on js file changes in './source/app'
   gulp.watch(path.join(dir.src, 'app/**/*.js'), ['app']);
@@ -133,37 +174,38 @@ gulp.task('watch', function () {
   // run `app` task on js file changes in './source/app'
   gulp.watch(path.join(dir.src, '**/*.{json,html,hbs,handlebars}'), ['grunt-assemble']);
 
+  // ping livereload sever when dist folder files change
+  gulp.watch(path.join(dir.dist, '**/*')).on('change', livereload.changed);
   livereload.listen();
-  gulp.watch('dist/**/*').on('change', livereload.changed);
 });
 
+
+// Task `compile`
+// runs blocks of build tasks in specific order 
 gulp.task('compile', function(done) {
   sequence(
-    'empty',
-    [
-      'sync',
-      'styles',
-      'scripts',
-      'icons',
-      'copy',
-    ],
+    ['empty', 'sync'],
+    ['styles','scripts','icons','copy'],
     'pages',
     done
   );
 });
 
+// Task `pages`
+// gets any ext. data, then assembles pages
+gulp.task('pages', function(done) {
+  sequence('pull-data', 'grunt-assemble', done);
+});
 
-gulp.task('sync', ['grunt-update_json']);
-gulp.task('scripts', ['lib', 'app']);
-gulp.task('copy', ['copy-images', 'copy-fonts']);
+// Task `icons`
+// compile svgs, then prepare fragment
 gulp.task('icons', function(done) {
   sequence('grunt-iconizr', 'post-iconizr', done);
 });
 
-gulp.task('pages', function(done) {
-  sequence('grunt-assemble', done);
-});
-
+gulp.task('sync', ['grunt-update_json']);
+gulp.task('scripts', ['lib', 'app']);
+gulp.task('copy', ['copy-images', 'copy-fonts']);
 
 gulp.task('develop', ['compile', 'watch']);
 gulp.task('default', ['develop']);
